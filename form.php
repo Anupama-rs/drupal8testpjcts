9808d85dfa81679e2a7256f6883c39b326d05043
RS <anupama.rs@capgemini.com>
2021-05-28 18:07:48 +0530

    First Commit

---
+++ form.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>An HTML FORM</title>
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+$var="Hello Visual Studio";
+echo $var;
+echo "<br>";
+echo "this is my php file" .$var;
+?>
